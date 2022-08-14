ae4c792c66e5f2ccc416fe1938c01835d50e63e1
Ruslanhio <ruslanhio2891@gmail.com>
2022-08-14 13:12:20 +0300

    nnewCommit

--- 06.js
+++ 06.js
@@ -1,4 +1,19 @@
-function timesFive(n) {
-  return n * 9;
+const  myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
 }
-const answer = timesFive(9);
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
